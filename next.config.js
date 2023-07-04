861eedf23a8dffb9db9789c8c6e6d259a7eb7d2d
wontae <wjs152828@gmail.com>
2023-07-05 03:18:40 +0900

    edit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
+/**
+* @type {import('next').NextConfig}
+*/
+const nextConfig = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
+};
+
+export default nextConfig;
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
