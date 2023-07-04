33b7c77d1f8a000e490b92072042d580c45256fe
wontae <wjs152828@gmail.com>
2023-07-05 05:22:06 +0900

    ready to deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,3 @@
-/**
-* @type {import('next').NextConfig}
-*/
-const nextConfig = {
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
-  assetPrefix: './',
-};
-
-export default nextConfig;
-
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
