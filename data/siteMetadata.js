fe66ef30f8eef98310e706433ef89dd8feb4f418
wontae <wjs152828@gmail.com>
2023-07-17 13:43:52 +0900

    setup SEO

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -22,11 +22,11 @@ const siteMetadata = {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: false, // true or false
-    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
-    posthogAnalyticsId: '', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
+    plausibleDataDomain: 'wontae.vercel.app', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    simpleAnalytics: true, // true or false
+    umamiWebsiteId: 'f9e16d2f-a282-4f1d-a82d-f40191a710d1', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    googleAnalyticsId: 'G-JD6F8XC4R7', // e.g. UA-000000-2 or G-XXXXXXX
+    posthogAnalyticsId: 'phc_t1ndV7tlJH5trIQIYdjdyFdL0msfejAk4Ppy4Cn3SnD', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
